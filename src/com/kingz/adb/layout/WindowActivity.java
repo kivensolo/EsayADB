ed46c95067f4107266f9589732983f6db2ad3faa
kivensolo <760553478@qq.com>
2018-06-22 15:43:52 +0800

    add open source Beautyeye.jar

--- src/com/kingz/adb/layout/WindowActivity.java
+++ src/com/kingz/adb/layout/WindowActivity.java
@@ -1,11 +1,19 @@
 package com.kingz.adb.layout;
 
 import com.kingz.adb.container.JMainFrame;
+import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
 
 import java.awt.*;
 
 public class WindowActivity {
     public static void main(String[] args) {
+        try {
+            //http://www.52im.net/thread-26-1-1.html
+            BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.generalNoTranslucencyShadow;
+            org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         // 开启一个线程，所有的Swing组件必须由事件分派线程进行配置，线程将鼠标点击和按键控制转移到用户接口组件。
         EventQueue.invokeLater(new Runnable() {
             @Override
